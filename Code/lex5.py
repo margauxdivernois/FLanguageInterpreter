7642363230f1dd6104dff2711a88256c700603e1
Lucas Beltrame <lucas.beltrame@he-arc.ch>
2015-11-27 10:59:03 +0100

    Changement du print en 'Affiche'

--- Code/lex5.py
+++ Code/lex5.py
@@ -3,58 +3,65 @@
 import ply.lex as lex
 
 reserved_words = (
-        'while',
-        'print'
+    'while',
+    'Affiche'
 )
 
 tokens = (
-	'NUMBER',
-	'ADD_OP',
-	'MUL_OP',
-	'ENDOFLINE',
-	'CROCHET_OPEN',
-    'CROCHET_CLOSE',
-	'AFFECTATION',
-	'VARIABLE'
-) + tuple(map(lambda s : s.upper(), reserved_words))
+             'NUMBER',
+             'ADD_OP',
+             'MUL_OP',
+             'ENDOFLINE',
+             'CROCHET_OPEN',
+             'CROCHET_CLOSE',
+             'AFFECTATION',
+             'VARIABLE'
+         ) + tuple(map(lambda s: s.upper(), reserved_words))
 ### AJOUTE LES ELEMENTS DE reserved_words
 
 literals = '()'
 
+
 def t_ADD_OP(t):
-	r'\plus|moins'
-	return t
-	
+    r'\plus|moins'
+    return t
+
+
 def t_MUL_OP(t):
-	r'\*|/'
-	return t
+    r'\*|/'
+    return t
+
 
 def t_NUMBER(t):
-	r'\d+(\.\d+)?'
-	try:
-		t.value = float(t.value)    
-	except ValueError:
-		print ("Line %d: Problem while parsing %s!" % (t.lineno,t.value))
-		t.value = 0
-	return t
+    r'\d+(\.\d+)?'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Line %d: Problem while parsing %s!" % (t.lineno, t.value))
+        t.value = 0
+    return t
+
 
 def t_ENDOFLINE(t):
-        r'~'
-        return t
+    r'~'
+    return t
+
 
 def t_AFFECTATION(t):
-        r'vaut'
-        return t
+    r'vaut'
+    return t
+
 
 def t_VARIABLE(t):
-	r'[A-Za-z_]\w*'
-	if t.value in reserved_words:
-                t.type = t.value.upper()
-	return t
-	
+    r'[A-Za-z_]\w*'
+    if t.value in reserved_words:
+        t.type = t.value.upper()
+    return t
+
+
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 
 def t_CROCHET_OPEN(t):
@@ -71,18 +78,19 @@ t_ignore = ' \t'
 
 
 def t_error(t):
-	print ("Illegal character '%s'" % t.value[0])
-	t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 lex.lex()
 
 if __name__ == "__main__":
-	import sys
-	#prog = open(sys.argv[1]).read()
-	prog = open("test.txt").read()
-	lex.input(prog)
+    import sys
+    # prog = open(sys.argv[1]).read()
+    prog = open("test.txt").read()
+    lex.input(prog)
 
-	while 1:
-		tok = lex.token()
-		if not tok: break
-		print ("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
+    while 1:
+        tok = lex.token()
+        if not tok: break
+        print("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
--- Code/parser5.py
+++ Code/parser5.py
@@ -4,77 +4,93 @@ import ply.yacc as yacc
 import AST
 from lex5 import tokens
 
+
 def p_programme_statement(p):
-        """ programme : statement """
-        p[0] = AST.ProgramNode(p[1])
+    """ programme : statement """
+    p[0] = AST.ProgramNode(p[1])
+
 
 def p_programme_recursive(p):
-        """ programme : statement ENDOFLINE programme"""
-        p[0] = AST.ProgramNode([p[1]]+p[3].children)
+    """ programme : statement ENDOFLINE programme"""
+    p[0] = AST.ProgramNode([p[1]] + p[3].children)
+
 
 def p_statement(p):
-	""" statement : affectation
+    """ statement : affectation
         | structure
         | printExpression"""
-	p[0] = p[1]
+    p[0] = p[1]
+
 
 def p_structure(p):
-        """structure : WHILE expression CROCHET_OPEN programme CROCHET_CLOSE"""
-        p[0] = AST.WhileNode([p[2],p[4]]);
+    """structure : WHILE expression CROCHET_OPEN programme CROCHET_CLOSE"""
+    p[0] = AST.WhileNode([p[2], p[4]]);
+
 
 def p_printExpression(p):
-        """ printExpression : PRINT expression"""
-        p[0] = AST.PrintNode(p[2])
+    """ printExpression : AFFICHE expression"""
+    p[0] = AST.PrintNode(p[2])
+
 
 def p_expression_affectation(p):
-        """affectation : VARIABLE AFFECTATION expression"""
-        p[0] = AST.AssignNode([AST.TokenNode(p[1]),p[3]])
+    """affectation : VARIABLE AFFECTATION expression"""
+    p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+
 
 def p_expression_variable(p):
-        """expression : VARIABLE"""
-        p[0] = AST.TokenNode(p[1])
-        
+    """expression : VARIABLE"""
+    p[0] = AST.TokenNode(p[1])
+
+
 def p_expression_op(p):
-	"""expression : expression ADD_OP expression
+    """expression : expression ADD_OP expression
 			| expression MUL_OP expression"""
-	p[0] = AST.OpNode(p[2],[p[1],p[3]])
-	
+    p[0] = AST.OpNode(p[2], [p[1], p[3]])
+
+
 def p_expression_num(p):
-	"""expression : NUMBER"""
-	p[0] = AST.TokenNode(p[1])
-	
+    """expression : NUMBER"""
+    p[0] = AST.TokenNode(p[1])
+
+
 def p_expression_paren(p):
-	"""expression : '(' expression ')' """
-	p[0] = p[2]
-	
+    """expression : '(' expression ')' """
+    p[0] = p[2]
+
+
 def p_minus(p):
-	"""expression : ADD_OP expression %prec UMINUS"""
-	p[0] = AST.OpNode(p[1],[p[2]])
+    """expression : ADD_OP expression %prec UMINUS"""
+    p[0] = AST.OpNode(p[1], [p[2]])
+
 
 def p_error(p):
-    print ("Syntax error in line %d" % p.lineno)
+    print("Syntax error in line %d" % p.lineno)
     yacc.yacc().errok()
 
+
 def parse(program):
-        return yacc.parse(program)
+    return yacc.parse(program)
+
 
 precedence = (
     ('left', 'ADD_OP'),
     ('left', 'MUL_OP'),
-    ('right', 'UMINUS'),  
+    ('right', 'UMINUS'),
 )
 
 yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
-	import sys
-	prog = open("test.txt").read()
-	#prog = open(sys.argv[1]).read()
-	result = yacc.parse(prog)
-	print(result)
+    import sys
 
-	import os
-	graph = result.makegraphicaltree()
-	name = os.path.splitext("test.txt")[0]+'-ast.pdf'
-	graph.write_pdf(name)
-	print("Wrote AST to", name)
+    prog = open("test.txt").read()
+    # prog = open(sys.argv[1]).read()
+    result = yacc.parse(prog)
+    print(result)
+
+    import os
+
+    graph = result.makegraphicaltree()
+    name = os.path.splitext("test.txt")[0] + '-ast.pdf'
+    graph.write_pdf(name)
+    print("Wrote AST to", name)
