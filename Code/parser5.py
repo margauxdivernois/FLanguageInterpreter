f4e9f4f445805b04c42ee3577c3057b19020e6bf
Lucas Beltrame <lucas.beltrame@he-arc.ch>
2015-12-11 11:05:12 +0100

    Ajout des tableaux dans le Lex Changement de mots clés (notamment ajout des accents)

--- Code/parser5.py
+++ Code/parser5.py
@@ -5,6 +5,7 @@ import ply.yacc as yacc
 import AST
 from lex5 import tokens
 
+
 def p_programme_statement(p):
     """ programme : statement """
     p[0] = AST.ProgramNode(p[1])
@@ -19,48 +20,56 @@ def p_statement(p):
         | printExpression"""
     p[0] = p[1]
 
-def p_structure(p):
-    """structure : """
 
 def p_expression_egal(p):
     """expression : expression EST EGAL A expression"""
-    p[0] = AST.OpNode(p[3],[p[1],p[5]])
+    p[0] = AST.OpNode(p[3], [p[1], p[5]])
+
 
 def p_expression_different(p):
     """expression : expression EST DIFFERENT DE expression"""
-    p[0] = AST.OpNode(p[3],[p[1],p[5]])
+    p[0] = AST.OpNode(p[3], [p[1], p[5]])
+
 
 def p_expression_superieur(p):
     """expression : expression EST SUPERIEUR A expression"""
-    p[0] = AST.OpNode(p[3],[p[1],p[5]])
+    p[0] = AST.OpNode(p[3], [p[1], p[5]])
+
 
 def p_expression_inferieur(p):
     """expression : expression EST INFERIEUR A expression"""
-    p[0] = AST.OpNode(p[3],[p[1],p[5]])
+    p[0] = AST.OpNode(p[3], [p[1], p[5]])
+
 
 def p_expression_inferieuregal(p):
     """expression : expression EST INFERIEUR OU EGAL A expression"""
-    p[0] = AST.OpNode(p[3]+p[5],[p[1],p[7]])
+    p[0] = AST.OpNode(p[3] + p[5], [p[1], p[7]])
+
 
 def p_expression_superieuregal(p):
     """expression : expression EST SUPERIEUR OU EGAL A expression"""
-    p[0] = AST.OpNode(p[3]+p[5],[p[1],p[7]])
+    p[0] = AST.OpNode(p[3] + p[5], [p[1], p[7]])
+
 
 def p_structure_while(p):
     """structure : TANT QUE expression CROCHET_OPEN programme CROCHET_CLOSE"""
     p[0] = AST.WhileNode([p[3], p[5]])
 
+
 def p_structure_si(p):
     """structure : SI expression ALORS ':' CROCHET_OPEN programme CROCHET_CLOSE"""
-    p[0] = AST.SiNode([p[2], p4[6]])
+    p[0] = AST.SiNode([p[2], p[6]])
+
 
 def p_printExpression(p):
     """ printExpression : AFFICHE expression"""
     p[0] = AST.PrintNode(p[2])
 
+
 def p_expression_affectation(p):
     """affectation : VARIABLE AFFECTATION expression"""
     p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+
 
 def p_expression_variable(p):
     """expression : VARIABLE"""
@@ -70,13 +79,16 @@ def p_expression_string(p):
     """expression : STRING"""
     p[0] = AST.TokenNode(p[1])
 
+
 def p_expression_opAdd(p):
     """expression : expression ADD_OP expression"""
-    p[0] = AST.OpNode(p[2],[p[1],p[3]])
+    p[0] = AST.OpNode(p[2], [p[1], p[3]])
+
 
 def p_expression_opMul(p):
     """expression : expression MUL_OP PAR expression"""
-    p[0] = AST.OpNode(p[2],[p[1],p[4]])
+    p[0] = AST.OpNode(p[2], [p[1], p[4]])
+
 
 def p_expression_num(p):
     """expression : NUMBER"""
@@ -85,6 +97,7 @@ def p_expression_num(p):
 def p_expression_paren(p):
     """expression : '(' expression ')' """
     p[0] = p[2]
+
 
 def p_minus(p):
     """expression : ADD_OP expression %prec UMINUS"""
@@ -99,8 +112,13 @@ def p_error(p):
     print(p)
     yacc.yacc().errok()
 
+def p_structure(p):
+    """structure : """
+
+
 def parse(program):
     return yacc.parse(program)
+
 
 precedence = (
     ('left', 'ADD_OP'),
@@ -112,13 +130,15 @@ yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
     import sys
+
     prog = open("test.txt").read()
-    #prog = open(sys.argv[1]).read()
+    # prog = open(sys.argv[1]).read()
     result = yacc.parse(prog)
     print(result)
 
     import os
+
     graph = result.makegraphicaltree()
-    name = os.path.splitext("test.txt")[0]+'-ast.pdf'
+    name = os.path.splitext("test.txt")[0] + '-ast.pdf'
     graph.write_pdf(name)
     print("Wrote AST to", name)
